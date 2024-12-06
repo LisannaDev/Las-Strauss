e26b8169e2466745292737f04e7837a0f98df0be
Natacha CREPU <lisannastrauss.dc@gmail.com>
2024-12-06 09:46:53 +0100

    Migrate to Vite

---
+++ LS_frontend/lsfrontend/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Utilisez './' si l'application est déployée dans un sous-répertoire.
+  build: {
+    outDir: 'dist',
+  },
+});
+
