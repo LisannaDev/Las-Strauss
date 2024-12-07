ade7a98a645addc55042edb206843c1825420fea
Natacha CREPU <lisannastrauss.dc@gmail.com>
2024-12-07 21:41:14 +0100

    Fix vite.config.ts

--- LS_frontend/lsfrontend/vite.config.ts
+++ LS_frontend/lsfrontend/vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Utilisez './' si l'application est déployée dans un sous-répertoire.
-  build: {
-    outDir: 'dist',
-  },
+    plugins: [react()],
+    base: './',
+    build: {
+        outDir: 'dist',
+    },
+    server: {
+        open: true,
+    },
 });
-
